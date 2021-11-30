9cc61665844d81f21325bd4d0698587107623d2c
Minju Kim <kmj673@gmail.com>
2021-11-30 23:39:03 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const testSample = { abc: 1, minju: 2, asdf: 3 };
+
+function capitaliseKeys(obj) {
+  let newObj = {};
+  //to find obj's key
+  for (const [key, value] of Object.entries(obj)) {
+    // to use to UpperCase
+    let newKey = key.toUpperCase();
+    // append the capitalised key and value pair to newObj
+    const testObj = new Object();
+    testObj.key;
+  }
+
+  //   return newObj;
+}
+
+capitaliseKeys(testSample);
+
+//globsl scope variable
+
+//classList syntax method
